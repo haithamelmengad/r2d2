84a0fd11c07fb0bf7b8e2b482f5e0cc5668488d2
Delaney Ugelstad <dugelsta@wellesley.edu>
2018-03-27 21:58:18 -0700

    authorize google calendar

--- models/models.js
+++ models/models.js
@@ -1,13 +1,98 @@
-var mongoose = require('mongoose');
+import mongoose from 'mongoose';
 var Schema = mongoose.Schema;
 
-// Mongoose model
+//Models a User
 var userSchema = new Schema({
-	owner: String,
+	slackUsername: String,
+	defaultMeetingMins: Number,
 	slackId: String,
-    googleId: String
-})
+	googleId: String,
+	slackEmail: String,
+	slackDmIds: Array
+});
 
 var User = mongoose.model('User', userSchema);
 
-module.exports = User;
+
+//Models a Reminder
+const reminderSchema = new mongoose.Schema({
+	subject: {
+		type: String,
+		required: true
+	},
+	day: {
+		type: Date,
+		required: true
+	},
+	googleCalEventId: {
+		type: Schema.Types.ObjectId
+		// ref: 
+	},
+	requesterId: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	}
+});
+
+const Reminder = mongoose.model('Reminder', reminderSchema);
+
+//Models a Meeting
+const meetingSchema = new mongoose.Schema({
+	day: {
+		type: Date,
+		required: true
+	},
+	time: {
+		type: Date, //time??
+		required: true
+	},
+	invitees: {
+		type: Array,
+		required: true
+	},
+	subject: String,
+	location: String,
+	meetingLength: String, //Hours??
+	googleCalFields: String, //Type??
+	status: {
+		type: String,
+		enum: ['Pending', 'Scheduled']
+	},
+	createdAt: Date, //type?
+	requesterId: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	}
+});
+
+const Meeting = mongoose.model('Meeting', meetingSchema);
+
+
+
+/**
+ * Models an invite request
+ */
+const inviteRequestSchema = new mongoose.Schema({
+	eventId: {
+		type: Schema.Types.ObjectId
+	},
+	inviteeId: {
+		type: Schema.Types.ObjectId
+	},
+	requesterId: {
+		type: Schema.Types.ObjectId
+	},
+	status: {
+		type: String,
+		enum: ['Pending', 'Scheduled']
+	}
+});
+
+const InviteRequest = mongoose.model('InviteRequest', inviteRequestSchema);
+
+
+module.exports = {
+	User,
+	Meeting,
+	Reminder
+};
